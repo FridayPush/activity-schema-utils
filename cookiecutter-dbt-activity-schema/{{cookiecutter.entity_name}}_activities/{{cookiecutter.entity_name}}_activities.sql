80b4be53bb97c9e31440369a80e6e93a34cce444
benwhite-birdiecare <55486471+benw-at-birdie@users.noreply.github.com>
2022-02-14 14:30:19 +0000

    Added cookiecutter template enabling incremental update of activity schema

---
+++ cookiecutter-dbt-activity-schema/{{cookiecutter.entity_name}}_activities/{{cookiecutter.entity_name}}_activities.sql
@@ -0,0 +1,101 @@
+{{"{{"}}
+  config(
+    materialized = 'incremental',
+    unique_key='activity_id',
+
+    post_hook=" -- Remove records from old versions of activity transformations.
+                BEGIN; 
+                delete from {{"{{"}} this }} 
+                where _activity_source in ('old_version_of_activity_transformation_to_delete_v0.1',
+                                           'old_version_of_activity_transformation_to_delete_v0.2') ;
+                COMMIT;"
+  )
+}}
+{{"{#"}} Consider adding clustering to the above config to improve performance - e.g. cluster_by=['to_date(ts)'] #}
+
+
+with new_activities as (
+
+  select 
+      *,
+
+      row_number() 
+          over (partition by customer, 
+                          activity
+              order by ts) as activity_occurrence,
+
+      lead(ts) 
+          over (partition by customer, 
+                          activity
+              order by ts) as activity_repeated_at
+
+  from {{"{{"}} ref('union_{{cookiecutter.entity_name}}_activities') }} 
+
+)
+
+
+{{"{%"}} if not is_incremental() %}
+  
+  select * from new_activities
+
+{{"{%"}} else %}
+
+  {{"{%"}} if adapter.get_relation(this.database, this.schema, '{{cookiecutter.entity_name}}_last_activities') is none %}
+  {{"{%"}}- do log('{{cookiecutter.entity_name}}_last_activities model not available, please run this before starting an incremental load.', info=True) -%}
+  {{"{%"}} endif %}
+
+  -- 1. Increment activity occurrence of newly created activities based on last activity before load was started.
+  select
+
+      new_activities.activity_id,
+      new_activities.ts,
+      new_activities.customer,
+      new_activities.activity,
+      new_activities.anonymous_customer_id,
+      new_activities.feature_1,
+      new_activities.feature_2,
+      new_activities.feature_3,
+      new_activities.revenue_impact,
+      new_activities.link,
+      new_activities.activity_occurrence + coalesce(last_recorded_activities.activity_occurrence, 1) - 1 as activity_occurrence,
+      new_activities.activity_repeated_at,
+      new_activities._activity_source
+
+  from new_activities 
+
+      left join {{"{{"}} adapter.get_relation(this.database, this.schema, '{{cookiecutter.entity_name}}_last_activities') }} as last_activities
+
+          on new_activities.customer = last_activities.customer 
+              and new_activities.activity = last_activities.activity 
+
+  where new_activities.activity_id != last_activities.last_activity_id -- exclude last_activity id (this will be inserted below)
+    or last_activities.last_activity_id is null
+
+
+  union all 
+
+  -- 2. Find activity_repeated_at for last occurrence of activity before incremental update
+  select 
+
+      last_activities.last_activity_id as activity_id,
+      last_activities.ts,
+      last_activities.customer,
+      last_activities.activity,
+      last_activities.anonymous_customer_id,
+      last_activities.feature_1,
+      last_activities.feature_2,
+      last_activities.feature_3,
+      last_activities.revenue_impact,
+      last_activities.link,
+      last_activities.activity_occurrence,
+      new_activities.ts as activity_repeated_at,
+      last_activities._activity_source
+
+  from {{"{{"}} adapter.get_relation(this.database, this.schema, '{{cookiecutter.entity_name}}_last_activities') }} as last_activities
+
+      join new_activities 
+          on  last_activities.customer = new_activities.customer 
+          and last_activities.activity = new_activities.activity 
+              and new_activities.activity_occurrence = 1
+
+{{"{%"}} endif %}
---
+++ cookiecutter-dbt-activity-schema/{{cookiecutter.entity_name}}_activities/{{cookiecutter.entity_name}}_activity_a_dictionary.sql
@@ -0,0 +1,26 @@
+{{"{#"}}
+    Create dictionary entries for activities to enable SQL users and the tools/functions they use to understand the contents of your activity schemas.
+#}
+
+{{"{{"}}
+  config(
+    materialized = 'ephemeral',
+    )
+}}
+
+select
+                                                            -- Examples: -
+    'activity_a' as activity,                               --   support_chat_started
+    'Description of activity a' as activity_description,    --   User started an intercom conversation with the support team.
+
+    'field_name_for_feature_1' as feature_1,                --   first_responded_at
+    'SQL DATA TYPE'            as feature_1_data_type,      --   TIMESTAMP
+    'Description of feature 1' as feature_1_description,    --   Timestamp recording when conversation was first responded to by support team.
+
+    'field_name_for_feature_2' as feature_2,                --   support_responses_count
+    'SQL DATA TYPE'            as feature_2_data_type,      --   NUMBER
+    'Description of feature 2' as feature_2_description,    --   Count of total responses from the support team require to resolve chat.
+
+    'field_name_for_feature_3' as feature_3,                --   satisfaction rating
+    'SQL DATA TYPE'            as feature_3_data_type,      --   TEXT
+    'Description of feature 3' as feature_3_description     --   User rating of the support they received, on a scale from 'Terrible' to 'Amazing'.
