a3c4a6c46c405948b921c9c38d901e2066f4f7ac
benwhite-birdiecare <ben.white@birdie.care>
2022-02-22 09:36:03 +0000

    Corrected incremental load calculations for activity_occurrence and activity_repeated_at fields.

--- cookiecutter-dbt-activity-schema/{{cookiecutter.entity_name}}_activities/{{cookiecutter.entity_name}}_activities.sql
+++ cookiecutter-dbt-activity-schema/{{cookiecutter.entity_name}}_activities/{{cookiecutter.entity_name}}_activities.sql
@@ -40,11 +40,37 @@ with new_activities as (
 
 {{"{%"}} else %}
 
-  {{"{%"}} if adapter.get_relation(this.database, this.schema, '{{cookiecutter.entity_name}}_last_activities') is none %}
-  {{"{%"}}- do log('{{cookiecutter.entity_name}}_last_activities model not available, please run this before starting an incremental load.', info=True) -%}
-  {{"{%"}} endif %}
+  -- get the first existing activity that is also present in the new activities
+  , first_activity_to_be_updated as (
 
-  -- 1. Increment activity occurrence of newly created activities based on last activity before load was started.
+    select 
+      existing_activities.customer,
+      existing_activities.activity,
+      existing_activities.activity_occurrence 
+
+    from new_activities
+
+      join {{"{{"}} adapter.get_relation(this.database, this.schema, '{{cookiecutter.entity_name}}_activities') }} as existing_activities
+      
+        on new_activities.customer = existing_activities.customer     -- Match on customer and activity to aid optimisation of join.
+          and new_activities.activity = existing_activities.activity  -- ... Can be removed if not helpful.
+          and new_activities.activity_id = existing_activities.activity_id
+
+    qualify row_number() over (partition by existing_activities.customer, existing_activities.activity
+                               order by existing_activities.activity_occurrence) = 1
+
+  )
+
+  , last_existing_activity as (
+
+    select 
+      *
+    from {{"{{"}} adapter.get_relation(this.database, this.schema, '{{cookiecutter.entity_name}}_activities') }}
+    where activity_repeated_at is null
+
+  )
+
+  -- 1. Increment activity occurrence of newly created activities based occurrences in existing data.
   select
 
       new_activities.activity_id,
@@ -57,45 +83,62 @@ with new_activities as (
       new_activities.feature_3,
       new_activities.revenue_impact,
       new_activities.link,
-      new_activities.activity_occurrence + coalesce(last_activities.activity_occurrence, 1) - 1 as activity_occurrence,
+
+      new_activities.activity_occurrence
+        + coalesce(first_activity_to_be_updated.activity_occurrence - 1,  -- If update overlaps with existing data then increment new occurrence counts
+                                                                          -- ... based on the existing occurrence of the first activity which is to be updated by new data.
+
+                   last_existing_activity.activity_occurrence)            -- If update doesn't overlap then increment new occurrences based on the occurrence of latest activity in the existing data.
+        
+        as activity_occurrence,
+
       new_activities.activity_repeated_at,
       new_activities._activity_source
 
   from new_activities 
 
-      left join {{"{{"}} adapter.get_relation(this.database, this.schema, '{{cookiecutter.entity_name}}_last_activities') }} as last_activities
+      left join first_activity_to_be_updated
+          on new_activities.customer = first_activity_to_be_updated.customer 
+              and new_activities.activity = first_activity_to_be_updated.activity 
 
-          on new_activities.customer = last_activities.customer 
-              and new_activities.activity = last_activities.activity 
-
-  where new_activities.activity_id != last_activities.last_activity_id -- exclude last_activity id (this will be inserted below)
-    or last_activities.last_activity_id is null
-
+      left join last_existing_activity
+          on new_activities.customer = last_existing_activity.customer 
+              and new_activities.activity = last_existing_activity.activity 
 
   union all 
 
-  -- 2. Find activity_repeated_at for last occurrence of activity before incremental update
+  -- 2. If new data doesn't overlap with existing data (i.e. there are no activies to be updated)
+  --    ... then update the activity_repeated_at of the last activity in the existing data set
+  --    ... with the timestamp of the first activity from the new data.
   select 
 
-      last_activities.last_activity_id as activity_id,
-      last_activities.ts,
-      last_activities.customer,
-      last_activities.activity,
-      last_activities.anonymous_customer_id,
-      last_activities.feature_1,
-      last_activities.feature_2,
-      last_activities.feature_3,
-      last_activities.revenue_impact,
-      last_activities.link,
-      last_activities.activity_occurrence,
+      last_existing_activity.activity_id,
+      last_existing_activity.ts,
+      last_existing_activity.customer,
+      last_existing_activity.activity,
+      last_existing_activity.anonymous_customer_id,
+      last_existing_activity.feature_1,
+      last_existing_activity.feature_2,
+      last_existing_activity.feature_3,
+      last_existing_activity.revenue_impact,
+      last_existing_activity.link,
+      last_existing_activity.activity_occurrence,
       new_activities.ts as activity_repeated_at,
-      last_activities._activity_source
+      last_existing_activity._activity_source
 
-  from {{"{{"}} adapter.get_relation(this.database, this.schema, '{{cookiecutter.entity_name}}_last_activities') }} as last_activities
+  from last_existing_activity
 
-      join new_activities 
-          on  last_activities.customer = new_activities.customer 
-          and last_activities.activity = new_activities.activity 
-              and new_activities.activity_occurrence = 1
+      join new_activities
+          on  last_existing_activity.customer = new_activities.customer 
+          and last_existing_activity.activity = new_activities.activity 
+              and new_activities.activity_occurrence = 1 
+
+
+      left join first_activity_to_be_updated
+          on last_existing_activity.customer = first_activity_to_be_updated.customer 
+              and last_existing_activity.activity = first_activity_to_be_updated.activity 
+
+  where first_activity_to_be_updated.customer is null -- if this is null then there are no activities to be 
+                                                      -- ... updated for this customer and activity... i.e. no overlap.
 
 {{"{%"}} endif %}
